39a1d58276db25dc4daba5c36912903d5cceec41
Renardo Williams <rennwill@gmail.com>
2023-08-14 16:30:31 -0400

    initial migration version

--- models.py
+++ models.py
@@ -1,3 +1,29 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer,primary_key=True) 
+    email = Column(String)
+    
+    
+        
+class Songs(Base):
+    __tablename__ = "songs"
+    id = Column(Integer,primary_key=True) 
+    title = Column(String)
+    artist = Column(String)
+    genre = Column(String)
+  
+        
+class Playlist(Base): 
+    __tablename__ = "playlists"  
+    id = Column(Integer,primary_key=True) 
+    name = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    
+    
+    
+    
